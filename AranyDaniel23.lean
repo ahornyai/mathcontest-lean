440ec6487d7d8445008e6dc39e8f352919921494
Alex <ahornyai@gmail.com>
2025-05-28 04:20:35 +0200

    Create AranyDaniel23.lean

---
+++ AranyDaniel23.lean
@@ -0,0 +1,178 @@
+import Mathlib.Tactic
+
+/-
+Arany Dániel 2013/2014 Kezdők II. kategória, III. forduló III. feladat
+
+Határozzuk meg az összes olyan p prímszámot, melyekre az
+x^4 + 4 = p*y^4
+egyenlet megoldható az egész számok körében
+----------------------
+Bizonyítsuk, hogy a megoldáshalmaz: (p, x, y) ∈ {(5, 1, 1), (5, 1, -1), (5, -1, 1), (5, -1, -1)}
+-/
+def SolutionSet : Set (ℤ × ℤ × ℤ) := {(5, 1, 1), (5, 1, -1), (5, -1, 1), (5, -1, -1)}
+
+lemma eq_not_solvable_if_both_even {p x y : ℤ} (hp : Prime p) (hx_even : 2 ∣ x) (hy_even : 2 ∣ y) : x^4 + 4 ≠ p*y^4 := by
+  intro h
+  
+  have h2 : 16 ∣ p*y^4 := by
+    obtain ⟨k, hk⟩ := hy_even
+
+    have : p * (2 * k) ^ 4 = 16*(p*k^4) := by ring
+    rw [hk, this]
+
+    exact Int.dvd_mul_right 16 (p * k ^ 4)
+  
+  have h2_contra : ¬ 16 ∣ p*y^4 := by
+    by_contra! h3
+    obtain ⟨k, hk⟩ := hx_even
+    have : (2 * k) ^ 4 = 16*(k^4) := by ring
+
+    rw [← h, hk, this] at h3
+
+    omega
+    
+  contradiction
+
+theorem arany2014_beginner_ii_iii_iii (p x y : ℤ) (hp : Prime p) (hp_pos : p > 0) : x^4 + 4 = p*y^4 ↔ ⟨p, x, y⟩ ∈ SolutionSet := by
+  unfold SolutionSet
+  simp
+  
+  constructor
+  · intro h
+
+    have : ∃n : ℕ, p=n := by exact Int.eq_ofNat_of_zero_le (by linarith)
+    obtain ⟨pn, hpn⟩ := this
+
+    have hpn_prime : Nat.Prime pn := by
+      refine Nat.prime_iff_prime_int.mpr ?_
+      rw [← hpn]
+      exact hp
+    
+    by_cases h1: p=2
+    · have hrhs_even : 2 ∣ p*y^4 := by
+        rw [h1]
+        exact Int.dvd_mul_right 2 (y ^ 4)
+      
+      have hx_even : 2 ∣ x^4 := by omega
+      rw [Prime.dvd_pow_iff_dvd (by exact Int.prime_two) (by norm_num)] at hx_even
+
+      have hy_even : 2 ∣ y := by
+        have hlhs_div_4 : 4 ∣ x^4 + 4 := by
+          obtain ⟨k, hk⟩ := hx_even
+          rw [hk]
+          
+          refine (Int.dvd_add_right ?_).mpr ?_
+          have : (2*k)^4 = 4*(4*k^4) := by ring
+          rw [this]
+          exact Int.dvd_mul_right 4 (4 * k ^ 4)
+          exact Int.dvd_of_emod_eq_zero rfl
+        
+        obtain ⟨k, hk⟩ := hlhs_div_4
+        rw [h1, hk] at h
+        
+        have : 2 ∣ y^4 := by omega
+        rw [Prime.dvd_pow_iff_dvd (by exact Int.prime_two) (by norm_num)] at this
+
+        exact this
+      
+      have : x ^ 4 + 4 ≠ p * y ^ 4 := by exact eq_not_solvable_if_both_even hp hx_even hy_even
+
+      contradiction
+    · by_cases h2: x % 2 = 0
+      · by_cases h3: y % 2 = 0
+        · have : x ^ 4 + 4 ≠ p * y ^ 4 := by
+            apply eq_not_solvable_if_both_even hp
+            exact Int.dvd_of_emod_eq_zero h2
+            exact Int.dvd_of_emod_eq_zero h3
+          
+          contradiction
+        · have h4 : Even (x ^ 4 + 4) := by
+            have : 2 ∣ x := by exact Int.dvd_of_emod_eq_zero h2
+            obtain ⟨k, hk⟩ := this
+            have : (2 * k) ^ 4 + 4 = 2*(8*k^4 + 2) := by ring
+            
+            rw [hk, this]
+            exact even_two_mul (8 * k ^ 4 + 2)
+          
+          have h4_contra : ¬ Even (x^4 + 4) := by
+            refine Int.not_even_iff_odd.mpr ?_
+            rw [h]
+
+            refine Odd.mul ?_ ?_
+            rw [hpn]
+            norm_num
+            refine Nat.Prime.odd_of_ne_two hpn_prime (by omega)
+            refine Odd.pow ?_
+            refine Int.odd_iff.mpr ?_
+            exact Int.emod_two_ne_zero.mp h3
+          
+          contradiction
+      · by_cases h3: y % 2 = 0
+        · have h4 : Odd (x ^ 4 + 4) := by
+            refine Odd.add_even ?_ ?_
+            refine Odd.pow ?_
+            refine Int.odd_iff.mpr ?_
+            exact Int.emod_two_ne_zero.mp h2
+            exact Int.even_iff.mpr rfl
+          
+          have h4_contra : ¬ Odd (x^4 + 4) := by
+            refine Int.not_odd_iff_even.mpr ?_
+            rw [h]
+
+            have : 2 ∣ y := by exact Int.dvd_of_emod_eq_zero h3
+            obtain ⟨k, hk⟩ := this
+            have : p*(2 * k) ^ 4 = 2*(8*k^4*p) := by ring
+            
+            rw [hk, this]
+            exact even_two_mul (8 * k ^ 4 * p)
+          
+          contradiction
+        · have h5 : (x^2 - 2*x + 2)*(x^2 + 2*x + 2) = p*y^4 := by linarith
+          
+          have : IsCoprime (x^2 - 2*x + 2) (x^2 + 2*x + 2) := by
+            refine Int.isCoprime_iff_gcd_eq_one.mpr ?_
+            refine Int.gcd_eq_one_iff.mpr ?_
+            intro c hc1 hc2
+
+            have hc3 : c ∣ (x^2 + 2*x + 2) - (x^2 - 2*x + 2) := by exact Int.dvd_sub hc2 hc1
+            have : (x^2 + 2*x + 2) - (x^2 - 2*x + 2) = 4*x := by ring
+            rw [this] at hc3
+
+            have h4ndivc : ¬ 4 ∣ c := by
+              have : ¬ 2 ∣ c := by
+                by_contra!
+                have : 2 ∣ x ^ 2 - 2 * x + 2 := by exact Int.dvd_trans this hc1
+                
+                have h6 : Even (p*y^4) := by
+                  refine Int.even_iff.mpr ?_
+                  obtain ⟨k, hk⟩ := this
+
+                  rw [← h5, hk, mul_assoc]
+                  exact Int.mul_emod_right 2 (k * (x ^ 2 + 2 * x + 2))
+                
+                have h6_contra : ¬ Even (p*y^4) := by
+                  refine Int.not_even_iff_odd.mpr ?_
+                  refine Odd.mul ?_ ?_
+                  rw [hpn]
+                  norm_cast
+                  refine Nat.Prime.odd_of_ne_two hpn_prime ?_
+                  omega
+                  refine Odd.pow ?_
+                  refine Int.odd_iff.mpr ?_
+                  exact Int.emod_two_ne_zero.mp h3
+                
+                contradiction
+              
+              omega
+            
+            have : c ∣ x := by
+              sorry
+            
+            sorry
+
+          sorry
+  · intro h
+    rcases h with h | h | h | h
+    all_goals
+      obtain ⟨rfl, rfl, rfl⟩ := h
+      norm_num
